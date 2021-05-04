0ba72a0e46f2e5aa408ddcf5159bafae98dbd0ec
Subhash Prajapati <subhash.prajapati@techifysolutions.com>
2021-05-04 13:17:05 +0530

    "technician app changes"

--- app/src/main/java/com/walkins/technician/activity/CompletedVisualDetailActivity.kt
+++ app/src/main/java/com/walkins/technician/activity/CompletedVisualDetailActivity.kt
@@ -8,10 +8,14 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.walkins.technician.R
 import com.walkins.technician.adapter.PendingTyreSuggestionAdpater
 import com.walkins.technician.common.TyreDetailCommonClass
+import com.walkins.technician.common.TyreKey
 import com.walkins.technician.common.onClickAdapter
+import java.lang.Exception
 
 class CompletedVisualDetailActivity : AppCompatActivity(), onClickAdapter, View.OnClickListener {
 
@@ -25,6 +29,13 @@ class CompletedVisualDetailActivity : AppCompatActivity(), onClickAdapter, View.
     private var tyreSuggestionAdapter: PendingTyreSuggestionAdpater? = null
     private var tvTitle: TextView? = null
     private var ivBack: ImageView? = null
+    private var ivSelectedMake: ImageView? = null
+    private var tvSelectedPattern: TextView? = null
+    private var tvSelectedSize: TextView? = null
+    private var tvManufacturingDate: TextView? = null
+    private var tvPsiIn: TextView? = null
+    private var tvPsiOut: TextView? = null
+    private var tvWeight: TextView? = null
     private var title: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,12 +59,37 @@ class CompletedVisualDetailActivity : AppCompatActivity(), onClickAdapter, View.
         )
         issueResolvedRecycView?.adapter = tyreSuggestionAdapter
 
+        tvManufacturingDate = findViewById(R.id.tvManufacturingDate)
+        tvPsiIn = findViewById(R.id.tvPsiIn)
+        tvPsiOut = findViewById(R.id.tvPsiOut)
+        tvWeight = findViewById(R.id.tvWeight)
+        tvSelectedPattern = findViewById(R.id.tvSelectedPattern)
+        tvSelectedSize = findViewById(R.id.tvSelectedSize)
+        ivSelectedMake = findViewById(R.id.ivSelectedMake)
+
         if (intent != null) {
             if (intent.getStringExtra("title") != null) {
                 tvTitle?.text = intent.getStringExtra("title")
             }
         }
         ivBack?.setOnClickListener(this)
+
+        tvManufacturingDate?.isClickable = false
+        tvManufacturingDate?.isEnabled = false
+
+        tvManufacturingDate?.text = TyreDetailCommonClass.manufaturingDate
+        tvSelectedSize?.text = TyreDetailCommonClass.vehicleSize
+        tvSelectedPattern?.text = TyreDetailCommonClass.vehiclePattern
+
+        try {
+            Glide.with(this@CompletedVisualDetailActivity)
+                .load(TyreDetailCommonClass.vehicleMakeURL)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(R.drawable.placeholder)
+                .into(ivSelectedMake!!)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
         Log.e("getvaluess_all", TyreDetailCommonClass.tyreType!!)
         Log.e("getvaluess_all", TyreDetailCommonClass.vehicleMake!!)
@@ -76,6 +112,13 @@ class CompletedVisualDetailActivity : AppCompatActivity(), onClickAdapter, View.
         Log.e("getvaluess_all", "" + TyreDetailCommonClass.issueResolvedArr!!)
         Log.e("getvaluess_all", TyreDetailCommonClass.visualDetailPhotoUrl!!)
 
+        suggestionArr.clear()
+
+        for (i in TyreDetailCommonClass.issueResolvedArr?.indices!!){
+            suggestionArr.add(TyreDetailCommonClass.issueResolvedArr?.get(i)!!)
+        }
+
+        tyreSuggestionAdapter?.notifyDataSetChanged()
     }
 
     override fun onPositionClick(variable: Int, check: Int) {
